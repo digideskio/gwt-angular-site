b7c758597af18b7e4f6440345cd0673b5e5d21c8
Yuki Asayama <kyoken74@gmail.com>
2015-01-01 14:46:53 -0500

    Copyright statement updated

---
+++ gwt-angular-web/src/main/java/com/asayama/gwt/angular/site/client/FooterController.java
@@ -0,0 +1,14 @@
+package com.asayama.gwt.angular.site.client;
+
+import java.util.Date;
+
+import com.asayama.gwt.angular.client.Controller;
+
+public class FooterController implements Controller {
+
+    public static final Date TODAY = new Date();
+    
+    @Override
+    public void onControllerLoad() {
+    }
+}
