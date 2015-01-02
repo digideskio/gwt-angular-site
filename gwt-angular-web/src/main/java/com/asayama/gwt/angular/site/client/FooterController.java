dac9c845f9f5407f4c8a8d3f2384121fbc420ae5
Yuki Asayama <kyoken74@gmail.com>
2015-01-02 12:44:48 -0500

    Updated footer text

--- gwt-angular-web/src/main/java/com/asayama/gwt/angular/site/client/FooterController.java
+++ gwt-angular-web/src/main/java/com/asayama/gwt/angular/site/client/FooterController.java
@@ -1,14 +1,27 @@
 package com.asayama.gwt.angular.site.client;
 
-import java.util.Date;
-
 import com.asayama.gwt.angular.client.Controller;
+import com.asayama.gwt.resources.client.HtmlResource;
+import com.google.gwt.core.shared.GWT;
+import com.google.gwt.resources.client.ClientBundle;
 
 public class FooterController implements Controller {
 
-    public static final Date TODAY = new Date();
-    
+    public static final String FOOTER_HTML = FooterResources.INSTANCE.footer().getSafeUri().asString();
+
     @Override
     public void onControllerLoad() {
     }
+
+    public String getDate() {
+        return String.valueOf(System.currentTimeMillis());
+    }
+}
+
+interface FooterResources extends ClientBundle {
+    
+    public static final FooterResources INSTANCE = GWT.create(FooterResources.class);
+
+    @Source("Footer.html")
+    public HtmlResource footer();
 }
